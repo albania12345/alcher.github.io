73df98f8ca77c900d2c4e2c9f54f76826a19d81f
albania12345 <32225793+albania12345@users.noreply.github.com>
2017-09-23 21:25:32 +0300

    first commit

---
+++ test-site/scripts/main.js
@@ -0,0 +1,23 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/pig.jpg') {
+      myImage.setAttribute ('src','images/pig2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/pig.png');
+    }
+}var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML  = 'V.G.S.P., ' + myName;
+}if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML  = 'V.G.S.P., ' + storedName;
+}myButton.onclick = function() {
+  setUserName();
+}
